bcfb70b3498ba50f7c45497b23869ab88db33ad7
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-03-20 13:52:53 +0400

    changess

--- main.js
+++ main.js
@@ -2,10 +2,32 @@ const leftArrow = document.querySelector('.arrow.left')
 const rightArrow = document.querySelector('.arrow.right')
 const filmsContainer = document.querySelector(".films")
 
+
 rightArrow.addEventListener('click', () => {
     filmsContainer.scrollLeft += 300;
 })
 
 leftArrow.addEventListener('click', () => {
     filmsContainer.scrollLeft -= 300;
+})
+
+/* კატეგორიების ცვლადები, კატეგორიაზე დაჭერისას მის შესაბამისად
+ უნდა გამოდიოდეს 3 ფილმი*/
+
+const action = document.querySelector('.catg.action')
+const comedy = document.querySelector('.catg.comedy')
+const drama = document.querySelector('.catg.drama')
+const fantasy = document.querySelector('.catg.fantasy')
+const horror = document.querySelector('.catg.horror')
+const mystery = document.querySelector('.catg.mystery')
+const romance = document.querySelector('.catg.romance')
+
+const actn_films = document.querySelector('.actn_films')
+let comedy_films = document.querySelector('.comedy_films')
+
+comedy.addEventListener('click', () => {
+      action.style.backgroundColor = 'black';
+      comedy.style.backgroundColor = 'blue';
+      actn_films.style.display = 'none';
+      comedy_films.style.display = 'flex';
 })
