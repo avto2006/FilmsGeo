171fd3d252da1159a321c035f5f34acee3647b32
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-03-21 12:03:39 +0400

    changess

--- main.js
+++ main.js
@@ -23,11 +23,33 @@ const mystery = document.querySelector('.catg.mystery')
 const romance = document.querySelector('.catg.romance')
 
 const actn_films = document.querySelector('.actn_films')
-let comedy_films = document.querySelector('.comedy_films')
+const comedy_films = document.querySelector('.comedy_films')
+const drama_films = document.querySelector('.drama_films')
+const fantasy_films = document.querySelector('.fantasy_films')
+const horror_films = document.querySelector('.horror_films')
+const mystery_films = document.querySelector('.mystery_films')
+const romance_films = document.querySelector('.romance_films')
 
 comedy.addEventListener('click', () => {
       action.style.backgroundColor = 'black';
       comedy.style.backgroundColor = 'blue';
       actn_films.style.display = 'none';
       comedy_films.style.display = 'flex';
+      drama_films.style.display = 'none';
+      fantasy_films.style.display = 'none';
+      horror_films.style.display = 'none';
+      mystery_films.style.display = 'none';
+      romance_films.style.display = 'none';
+})
+
+comedy.addEventListener('click', () => {
+      action.style.backgroundColor = 'black';
+      comedy.style.backgroundColor = 'blue';
+      actn_films.style.display = 'none';
+      comedy_films.style.display = 'flex';
+      drama_films.style.display = 'none';
+      fantasy_films.style.display = 'none';
+      horror_films.style.display = 'none';
+      mystery_films.style.display = 'none';
+      romance_films.style.display = 'none';
 })
